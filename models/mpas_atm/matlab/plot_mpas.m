8257121c56b2c4a0843ee9eaf456bb4d30478b76
Tim Hoar <thoar@ucar.edu>
2011-09-16 13:00:37 -0600

    Now handles any variable, vertical level ... turned off the interpolated shading by default.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/mpas@5247 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 2f4f67f617a3b3e2476622eb9a6b753ce5fce225

--- models/mpas_atm/matlab/plot_mpas.m
+++ models/mpas_atm/matlab/plot_mpas.m
@@ -1,8 +1,10 @@
-function plot_mpas(fname)
+function plot_mpas(fname,varname,level)
 %% plot_mpas 
 % 
-% fname = '../data/mpas_output.2010-10-23_03:00:00.nc';
-% plot_mpas(fname)
+% fname   = '../data/mpas_output.2010-10-23_03:00:00.nc';
+% varname = 'theta';
+% level   = 1;
+% plot_mpas(fname,varname,level)
 %
 
 %% DART software - Copyright 2004 - 2011 UCAR. This open source software is
@@ -15,17 +17,41 @@ function plot_mpas(fname)
 % $Revision$
 % $Date$
 
+%% Get the information about the triangles.
 x   = nc_varget(fname,'xCell');
 y   = nc_varget(fname,'yCell');
 z   = nc_varget(fname,'zCell');
-c   = nc_varget(fname,'surface_pressure');
 tri = nc_varget(fname,'cellsOnVertex');
-h   = trisurf(tri,x,y,z,c);
+
+%% Get the information about the field to be plotted.
+
+[start, count] = find_hyperslab(fname,varname,level);
+c = nc_varget(fname,varname,start,count);
+h = trisurf(tri,x,y,z,c);
 
 set(h,'LineStyle','none')
 axis equal
 axis off
 colorbar
+% shading interp
+h = title({sprintf('%s level %d',varname,level),fname});
+set(h,'Interpreter','none');
 
-shading interp
+function [start, count] = find_hyperslab(fname,varname,level);
+% helper to retrieve just the hyperslab of interest
+varinfo = nc_getvarinfo(fname,varname);
+ndims   = length(varinfo.Dimension);
+start   = zeros(1,ndims);
+count   = zeros(1,ndims);
 
+for i = 1:ndims
+   diminfo = nc_getdiminfo(fname,varinfo.Dimension{i});
+   dimname = diminfo.Name;
+   switch lower(dimname)
+      case {'nvertlevels','nvertlevelsp1','nsoillevels'}
+         start(i) = level-1;
+         count(i) = 1;
+      otherwise
+         count(i) = varinfo.Size(i);
+   end
+end
