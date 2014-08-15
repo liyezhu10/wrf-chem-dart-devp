4e68245e0fdfd4b1980ffd40d6151c6122f52f50
Tim Hoar <thoar@ucar.edu>
2014-08-15 19:26:51 +0000

    No longer needed. Moving to a new strategy for testing.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/wrfHydro@7124 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: e0dd20047d41a64c3728064029571eca2f73d799

--- models/wrfHydro/work/tests/setMpRestarts.sh
+++
@@ -1,9 +0,0 @@
-cp /home/jamesmcc/DART/lanai/models/wrfHydro/work/tests/namelist.hrldas.NoahMP /home/jamesmcc/DART/lanai/models/wrfHydro/work/namelist.hrldas
-
-cp /home/jamesmcc/DART/lanai/models/wrfHydro/work/tests/hydro.namelist.NoahMP /home/jamesmcc/DART/lanai/models/wrfHydro/work/hydro.namelist
-
-cp /home/jamesmcc/DART/lanai/models/wrfHydro/work/tests/input.nml.noahMPWrfHydroPerfectRestart /home/jamesmcc/DART/lanai/models/wrfHydro/work/input.nml
-
-cp /home/jamesmcc/DART/lanai/models/wrfHydro/work/tests/noahMP.RESTART.2013091200_DOMAIN3 /home/jamesmcc/DART/lanai/models/wrfHydro/work/restart.nc 
-
-cp /home/jamesmcc/DART/lanai/models/wrfHydro/work/tests/noahMP.HYDRO_RST.2013-09-12_00:00_DOMAIN3 /home/jamesmcc/DART/lanai/models/wrfHydro/work/restart.hydro.nc 
--- models/wrfHydro/work/tests/setNoahRestarts.sh
+++
@@ -1,9 +0,0 @@
-cp /home/jamesmcc/DART/lanai/models/wrfHydro/work/tests/namelist.hrldas.Noah /home/jamesmcc/DART/lanai/models/wrfHydro/work/namelist.hrldas
-
-cp /home/jamesmcc/DART/lanai/models/wrfHydro/work/tests/hydro.namelist.Noah /home/jamesmcc/DART/lanai/models/wrfHydro/work/hydro.namelist
-
-cp /home/jamesmcc/DART/lanai/models/wrfHydro/work/tests/input.nml.noahWrfHydroPerfectRestart /home/jamesmcc/DART/lanai/models/wrfHydro/work/input.nml
-
-cp /home/jamesmcc/DART/lanai/models/wrfHydro/work/tests/noah.RESTART.2013091102_DOMAIN3 /home/jamesmcc/DART/lanai/models/wrfHydro/work/restart.nc 
-
-cp /home/jamesmcc/DART/lanai/models/wrfHydro/work/tests/noah.HYDRO_RST.2013-09-11_02:00_DOMAIN3 /home/jamesmcc/DART/lanai/models/wrfHydro/work/restart.hydro.nc 
