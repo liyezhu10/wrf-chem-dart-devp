c4dea31616fccc1e112b41e2252e6dfe87628be6
kershaw-dart <Helen>
2015-05-22 20:17:36 +0000

    Distributed version of obs_def_tpw_mod.f90.  Ave Arellano is going to be using these. Bitwise with the trunk on my machine for a 3 observation case.  (Lanai_bitwise = .true.) Currently I don't have any more observations to test until I get obs and wrf files from Ave.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/rma_wrf_release@7995 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: ca57b50f04c5d9161e10b3984db2594eb230f5e7

--- obs_def/obs_def_tpw_mod.f90
+++ obs_def/obs_def_tpw_mod.f90
@@ -31,14 +31,14 @@
 ! END DART PREPROCESS KIND LIST
 
 ! BEGIN DART PREPROCESS USE OF SPECIAL OBS_DEF MODULE
-!  use obs_def_tpw_mod, only : get_expected_tpw
+!  use obs_def_tpw_mod, only : get_expected_tpw_distrib
 ! END DART PREPROCESS USE OF SPECIAL OBS_DEF MODULE
 
 ! BEGIN DART PREPROCESS GET_EXPECTED_OBS_FROM_DEF
 !         case(AQUA_TOTAL_PRECIPITABLE_WATER,TERRA_TOTAL_PRECIPITABLE_WATER,GPS_TOTAL_PRECIPITABLE_WATER)
-!            call get_expected_tpw(state, location, obs_val, istatus)
+!            call get_expected_tpw_distrib(state_ens_handle, location, expected_obs, istatus)
 !         case(AMSR_TOTAL_PRECIPITABLE_WATER,MODIS_TOTAL_PRECIPITABLE_WATER)
-!            call get_expected_tpw(state, location, obs_val, istatus)
+!            call get_expected_tpw_distrib(state_ens_handle, location, expected_obs, istatus)
 ! END DART PREPROCESS GET_EXPECTED_OBS_FROM_DEF
 
 ! BEGIN DART PREPROCESS READ_OBS_DEF
@@ -74,14 +74,16 @@ use     location_mod, only : location_type, set_location, get_location, &
                              VERTISLEVEL, VERTISPRESSURE, VERTISSURFACE
 use time_manager_mod, only : time_type, read_time, write_time, &
                              set_time, set_time_missing
-use  assim_model_mod, only : interpolate
+use  assim_model_mod, only : interpolate_distrib
 use     obs_kind_mod, only : KIND_SURFACE_PRESSURE, KIND_SPECIFIC_HUMIDITY, &
                              KIND_TOTAL_PRECIPITABLE_WATER, KIND_PRESSURE
+use  data_structure_mod, only : ensemble_type, copies_in_window
+
 
 implicit none
 private
 
-public ::  get_expected_tpw
+public ::  get_expected_tpw_distrib
 
 ! version controlled file description for error handling, do not edit
 character(len=256), parameter :: source   = &
@@ -141,7 +143,7 @@ end subroutine initialize_module
 
 
 !------------------------------------------------------------------------------
-subroutine get_expected_tpw(state_vector, location, tpw, istatus)
+subroutine get_expected_tpw_distrib(state_ens_handle, location, tpw, istatus)
 
 !------------------------------------------------------------------------------
 ! Purpose:  To calculate total precipitable water in a column over oceans.
@@ -150,7 +152,7 @@ subroutine get_expected_tpw(state_vector, location, tpw, istatus)
 !    location:        Observation location
 !
 ! output parameters:
-!    tpw:     total amount of liquid water (in cm) if all atmospheric water 
+!    tpw:     total amount of liquid water (in cm) if all atmospheric water
 !               vapor in the column was condensed.
 !    istatus: 0 if ok, a positive value for error
 !------------------------------------------------------------------------------
@@ -158,10 +160,10 @@ subroutine get_expected_tpw(state_vector, location, tpw, istatus)
 !  updated by n. collins  14 june 2012
 !------------------------------------------------------------------------------
 
-real(r8),            intent(in)  :: state_vector(:)
+type(ensemble_type), intent(in)  :: state_ens_handle
 type(location_type), intent(in)  :: location
-real(r8),            intent(out) :: tpw
-integer,             intent(out) :: istatus
+real(r8),            intent(out) :: tpw(:)
+integer,             intent(out) :: istatus(:)
 
 ! local variables
 real(r8) :: lon, lat, height, obsloc(3)
@@ -169,18 +171,30 @@ type(location_type) :: location2
 
 ! we'll compute the midpoint value for each pressure range, so allocate one
 ! more than the number of expected values.
-real(r8) :: pressure(max_pressure_intervals+1), qv(max_pressure_intervals+1)
-real(r8) :: pressure_interval, psfc
+!real(r8) :: pressure(max_pressure_intervals+1), qv(max_pressure_intervals+1)
+real(r8), allocatable :: pressure(:, :), qv(:, :)
+real(r8), allocatable :: pressure_interval(:), psfc(:), temp(:) ! ens size
+
 integer  :: which_vert, k, lastk, first_non_surface_level
+integer, allocatable :: track_status(:), lastk_ens(:)
+logical, allocatable :: still_in(:)
+integer  :: e, ens_size
 
 if ( .not. module_initialized ) call initialize_module
 
+ens_size = copies_in_window(state_ens_handle)
+
+allocate(pressure(ens_size, max_pressure_intervals+1), qv(ens_size, max_pressure_intervals+1))
+allocate(pressure_interval(ens_size), psfc(ens_size), lastk_ens(ens_size), track_status(ens_size))
+allocate(temp(ens_size), still_in(ens_size))
+
 ! start assuming an error.  if we return early these will already
-! be set.  replace the 99 error code with more specific values, 
+! be set.  replace the 99 error code with more specific values,
 ! and then 99 will only be returned in case of an uncaught error.
 
-tpw = missing_r8
-istatus = 99
+tpw(:) = missing_r8
+istatus(:) = 99
+track_status(:) = 99
 
 ! check for bad values in the namelist which exceed the size of the
 ! arrays that are hardcoded here.
@@ -211,17 +225,28 @@ location2 = set_location(lon, lat, height,  which_vert)
 ! assumes the values returned from the interpolation will be in these units:
 !   surface pressure :  Pa
 !   moisture         :  kg/kg
-call interpolate(state_vector, location2, KIND_SURFACE_PRESSURE, pressure(1), istatus)
-if (istatus /= 0) then
-   return
-endif
-call interpolate(state_vector, location2, KIND_SPECIFIC_HUMIDITY, qv(1), istatus)
-if (istatus /= 0) then
+call interpolate_distrib(location2, KIND_SURFACE_PRESSURE, istatus, pressure(:, 1), state_ens_handle)
+do e = 1, ens_size
+   if (istatus(e) /= 0) then
+      tpw(e) = missing_r8
+   endif
+enddo
+track_status = istatus
+if (all(track_status /=0)) then
    return
 endif
 
+
+call interpolate_distrib(location2, KIND_SPECIFIC_HUMIDITY, istatus, qv(:, 1), state_ens_handle)
+do e = 1, ens_size
+   if (istatus(e) /= 0) then
+      tpw(e) = missing_r8
+      track_status(e) = istatus(e)
+   endif
+enddo
+
 ! save this for use below
-psfc = pressure(1)
+psfc(:) = pressure(:, 1)
 
 ! there are two options for constructing the column of values.  if 'model_levels'
 ! is true, we query the model by vertical level number.  the 'separate_surface_level'
@@ -229,7 +254,7 @@ psfc = pressure(1)
 ! surface or if the surface values are a separate quantity below the 3d grid.
 
 if (model_levels) then
-   
+
    ! some models have a 3d grid of values and the lowest level contains
    ! the surface quantities.  others have a separate field for the
    ! surface values and the 3d grid starts at some given elevation.
@@ -250,78 +275,124 @@ if (model_levels) then
    ! (this is not a fatal error unless the first call fails).
    ! also exit the loop if the pressure is above the namelist-specified pressure top
 
-   lastk = 2
+   lastk_ens(:) = 2
+   still_in(:) = .true.
+
+   lastk = 2 ! Last k could be different for each ensmeble member
    LEVELS: do k=first_non_surface_level, 10000   ! something unreasonably large
-   
-      ! call the model_mod to get the pressure and specific humidity at each level 
+
+      ! call the model_mod to get the pressure and specific humidity at each level
       ! from the model and fill out the pressure and qv arrays.  the model must
       ! support a vertical type of level number.
 
       which_vert = VERTISLEVEL
       location2 = set_location(lon, lat, real(k, r8),  which_vert)
 
-      call interpolate(state_vector, location2, KIND_PRESSURE, pressure(lastk), istatus)
-      if (istatus /= 0 .or. pressure(lastk) < pressure_top) exit LEVELS
+      call interpolate_distrib(location2, KIND_PRESSURE, istatus, temp, state_ens_handle)
+      ! load up ensemble members still in
+      do e = 1, ens_size
+         if(still_in(e)) then
+            pressure(e, lastk) = temp(e)
+            if (istatus(e) /= 0) still_in(e) = .false.
+            if (pressure(e, lastk) < pressure_top) still_in = .false.
+         endif
+      enddo
 
-      call interpolate(state_vector, location2, KIND_SPECIFIC_HUMIDITY, qv(lastk), istatus)
-      if (istatus /= 0) return
-   
+      call interpolate_distrib(location2, KIND_SPECIFIC_HUMIDITY, istatus, temp, state_ens_handle)
+      do e = 1, ens_size
+         if (still_in(e)) then
+            qv(e, lastk) = temp(e)
+            lastk_ens(e) = lastk
+          endif
+      enddo
+
+      ! check to see if all ensemble members have failed
+      if (all(still_in .eqv. .false.)) then
+         exit LEVELS
+      endif
+
       lastk = lastk + 1
    enddo LEVELS
 
-   lastk = lastk - 1
-
    ! if we got no valid values, set istatus and return here.
    ! 'tpw' return value is already set to missing_r8
-   if (lastk == 1) then 
-      istatus = 3
-      return
-   endif
+   do e = 1, ens_size
+      if (lastk_ens(e) == 1) then ! ensemble member failed
+         track_status(e) = 3
+         !return ! can't return yet
+      endif
+   enddo
 
 else
 
    ! construct an explicit pressure column and get qv at each pressure level.
    ! each column will be at a different set of heights because it
-   ! divides the surface pressure and 'pressure_top' evenly into 
+   ! divides the surface pressure and 'pressure_top' evenly into
    ! 'num_pressure_intervals'.  so each column will have the same
    ! number of samples, but each may be at different pressure values
    ! depending on the surface pressure.
-   pressure_interval = (psfc - pressure_top)/num_pressure_intervals
-   lastk = num_pressure_intervals + 1
-   
+   pressure_interval(:) = (psfc(:) - pressure_top)/num_pressure_intervals
+   lastk = num_pressure_intervals + 1  ! This is the same for each ensemble member?
+   lastk_ens(:) = lastk
+
    ! construct a pressure column at fixed pressure intervals
    ! pressure(1) is always the surface pressure.
    do k=2, lastk
-      pressure(k) =  pressure(1) - pressure_interval * (k-1)
+      pressure(:, k) =  pressure(:, 1) - pressure_interval(:)* (k-1)
    end do
-   
+
    ! call the model_mod to get the specific humidity at each location from the model
    ! and fill out the qv array.
-   do k=2, lastk
+   do e = 1, ens_size  ! The location is set by pressure so there is a loop around the enesemble
+                       ! Would be better to not loop around
+      do k=2, lastk
 
-      which_vert = VERTISPRESSURE
-      location2 = set_location(lon, lat, pressure(k),  which_vert)
-   
-      call interpolate(state_vector, location2,  KIND_SPECIFIC_HUMIDITY, qv(k), istatus)
-      if (istatus /= 0) return
-   
+         which_vert = VERTISPRESSURE
+         location2 = set_location(lon, lat, pressure(e, k),  which_vert)
+
+         call interpolate_distrib(location2,  KIND_SPECIFIC_HUMIDITY, istatus, temp, state_ens_handle)
+         if(istatus(e) == 0 ) then ! fill out the qv array for this ensemble member
+            qv(e, k) = temp(e)
+         endif
+
+         if(istatus(e) /= 0 ) then ! fail
+            track_status(e) = istatus(e)
+         endif
+      enddo
+
    enddo
+
 endif
 
 ! whichever way the column was made (pressure levels or model levels),
 ! sum the values in the column, computing the area under the curve.
 ! pressure is in pascals (not hPa or mb), and moisture is in kg/kg.
+
 tpw = 0.0
-do k=1, lastk - 1
-   tpw = tpw + 0.5 * (qv(k) + qv(k+1) ) * (pressure(k) - pressure(k+1) )  
+do e = 1, ens_size
+   do k=1, lastk_ens(e) - 1
+      if (track_status(e) == 0) then
+         tpw(e) = tpw(e) + 0.5 * (qv(e, k) + qv(e, k+1) ) * (pressure(e, k) - pressure(e, k+1) )
+      endif
+   enddo
 enddo
 
-! convert to centimeters of water and return
-tpw = 100.0 * tpw /(density*gravity)   ! -> cm
+do e = 1, ens_size
+   if (track_status(e) == 0) then
+      tpw(e) = 100.0 * tpw(e) /(density*gravity)   ! -> cm
+   else
+      tpw(e) = missing_r8
+   endif
+enddo
 
-istatus = 0
+istatus(:) = track_status(:)
 
-end subroutine get_expected_tpw
+
+deallocate(pressure, qv)
+deallocate(pressure_interval, psfc, lastk_ens, track_status)
+deallocate(temp, still_in)
+
+end subroutine get_expected_tpw_distrib
 
 
 end module obs_def_tpw_mod
