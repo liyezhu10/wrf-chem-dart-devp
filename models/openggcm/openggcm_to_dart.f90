1d5cc42d3e6d543f305c9a58778b9f8f04626380
Tim Hoar <thoar@ucar.edu>
2016-02-23 00:12:41 +0000

    Can convert a binary file to netCDF ... need coordinate arrays.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/rma_openggcm@9838 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 17e1a5761e67fa939ec2d9f6fe838b4f81aba51f

--- models/openggcm/openggcm_to_dart.f90
+++ models/openggcm/openggcm_to_dart.f90
@@ -4,7 +4,7 @@
 !
 ! $Id$
 
-program openggcm_to_dart
+program openggcm_to_netcdf
 
 !----------------------------------------------------------------------
 ! purpose: interface between openggcm and DART
@@ -15,18 +15,16 @@ program openggcm_to_dart
 !         The output is a "DART restart file" format.
 ! 
 ! USAGE:  The openggcm filename is read from the openggcm_in namelist
-!         <edit openggcm_to_dart_output_file in input.nml:openggcm_to_dart_nml>
-!         openggcm_to_dart
+!         <edit openggcm_to_netcdf_output_file in input.nml:openggcm_to_netcdf_nml>
+!         openggcm_to_netcdf
 !
 ! author: Tim Hoar 6/24/09
 !----------------------------------------------------------------------
 
-use        types_mod, only : r8
+use        types_mod, only : r4
 use    utilities_mod, only : initialize_utilities, finalize_utilities, &
-                             find_namelist_in_file, check_namelist_read
-use        model_mod, only : restart_file_to_sv, static_init_model, &
-                             get_model_size, get_openggcm_restart_filename
-use state_vector_io_mod, only : awrite_state_restart, open_restart_write, close_restart
+                             find_namelist_in_file, check_namelist_read, &
+                             open_file, close_file, nc_check
 use time_manager_mod, only : time_type, print_time, print_date
 
 use netcdf
@@ -42,63 +40,76 @@ character(len=128), parameter :: revdate  = "$Date$"
 ! namelist parameters with default values.
 !-----------------------------------------------------------------------
 
-character (len = 128) :: openggcm_to_dart_output_file  = 'dart_ics'
+character(len=256) :: openggcm_to_netcdf_input_file  = '../data/da0002.dart.pot'
+character(len=256) :: openggcm_to_netcdf_output_file = 'openggcm.nc'
 
-namelist /openggcm_to_dart_nml/ openggcm_to_dart_output_file
+namelist /openggcm_to_netcdf_nml/ openggcm_to_netcdf_output_file, &
+                                  openggcm_to_netcdf_input_file
 
 !----------------------------------------------------------------------
 ! global storage
 !----------------------------------------------------------------------
 
-integer               :: io, iunit, x_size
+integer               :: io, iunit
 type(time_type)       :: model_time
-real(r8), allocatable :: statevector(:)
-character (len = 128) :: openggcm_restart_filename = 'no_openggcm_restart_filename' 
+real(r4), allocatable :: statevector(:)
+real(r4), allocatable :: datmat(:,:)
+
+integer :: nthe, nphi
+
+integer :: ncid, NtheDimID, NphiDimID, VarID
 
 !----------------------------------------------------------------------
 
-call initialize_utilities(progname='openggcm_to_dart')
+call initialize_utilities(progname='openggcm_to_netcdf')
 
 !----------------------------------------------------------------------
 ! Call model_mod:static_init_model(), which reads the namelists
 ! to set calendar type, starting date, deltaT, etc.
 !----------------------------------------------------------------------
 
-call static_init_model()
-
 !----------------------------------------------------------------------
 ! Read the namelist to get the input and output filenames.
 !----------------------------------------------------------------------
 
-call find_namelist_in_file("input.nml", "openggcm_to_dart_nml", iunit)
-read(iunit, nml = openggcm_to_dart_nml, iostat = io)
-call check_namelist_read(iunit, io, "openggcm_to_dart_nml") ! closes, too.
-
-call get_openggcm_restart_filename( openggcm_restart_filename )
+call find_namelist_in_file("input.nml", "openggcm_to_netcdf_nml", iunit)
+read(iunit, nml = openggcm_to_netcdf_nml, iostat = io)
+call check_namelist_read(iunit, io, "openggcm_to_netcdf_nml") ! closes, too.
 
 write(*,*)
-write(*,'(''openggcm_to_dart:converting openggcm restart file '',A, &
+write(*,'(''openggcm_to_netcdf:converting openggcm restart file '',A, &
       &'' to DART file '',A)') &
-       trim(openggcm_restart_filename), trim(openggcm_to_dart_output_file)
+       trim(openggcm_to_netcdf_input_file), trim(openggcm_to_netcdf_output_file)
 
-!----------------------------------------------------------------------
-! Now that we know the names, get to work.
-!----------------------------------------------------------------------
 
-x_size = get_model_size()
-allocate(statevector(x_size))
-call restart_file_to_sv(openggcm_restart_filename, statevector, model_time) 
+iunit = open_file(openggcm_to_netcdf_input_file, form='unformatted',action='read')
+read(iunit) nphi, nthe
+write(*,*)'TJH nphi, nthe is ',nphi, nthe
+allocate(statevector(nphi*nthe))
+read(iunit) statevector
+call close_file(iunit)
 
-iunit = open_restart_write(openggcm_to_dart_output_file)
+write(*,*)'file size should be ',4 + 4+4 + 4 + 4 + nphi*nthe*4 + 4
 
-call awrite_state_restart(model_time, statevector, iunit)
-call close_restart(iunit)
+call nc_check(nf90_create(openggcm_to_netcdf_output_file, NF90_CLOBBER, ncid),'openggcm_to_netcdf')
 
-call print_date(model_time, str='openggcm_to_dart:openggcm  model date')
-call print_time(model_time, str='openggcm_to_dart:DART model time')
-call finalize_utilities('openggcm_to_dart')
+! define dimensions
 
-end program openggcm_to_dart
+call nc_check(nf90_def_dim(ncid, 'nphi', nphi, NphiDimID),'openggcm_to_netcdf')
+call nc_check(nf90_def_dim(ncid, 'nthe', nthe, NtheDimID),'openggcm_to_netcdf')
+call nc_check(nf90_def_var(ncid, 'pot', nf90_real, (/ NphiDimID, NtheDimID /), VarID),'openggcm_to_netcdf')
+call nc_check(nf90_enddef(ncid),'openggcm_to_netcdf')
+
+datmat = reshape(statevector, (/ nphi, nthe /))
+call nc_check(nf90_put_var(ncid,  VarID,  datmat),'openggcm_to_netcdf')
+
+call nc_check(nf90_close(ncid),'openggcm_to_netcdf')
+
+!call print_date(model_time, str='openggcm_to_netcdf:openggcm  model date')
+!call print_time(model_time, str='openggcm_to_netcdf:DART model time')
+call finalize_utilities('openggcm_to_netcdf')
+
+end program openggcm_to_netcdf
 
 ! <next few lines under version control, do not edit>
 ! $URL$
