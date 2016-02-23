919ca0a95ee2cf03ab7a8d77ce78df7415222bf5
Tim Hoar <thoar@ucar.edu>
2016-02-23 18:58:59 +0000

    New data file that has a time record. The year is year since 1900. The netCDF file has the time in it - as 'days since' ... I need to check the math, but the machinery is right.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/rma_openggcm@9859 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 0f5e70abf5b60df0f3d8ccaa2acc81542ac5697b

--- models/openggcm/openggcm_to_dart.f90
+++ models/openggcm/openggcm_to_dart.f90
@@ -21,11 +21,12 @@ program openggcm_to_netcdf
 ! author: Tim Hoar 6/24/09
 !----------------------------------------------------------------------
 
-use        types_mod, only : r4, r8
+use        types_mod, only : r4, r8, digits12
 use    utilities_mod, only : initialize_utilities, finalize_utilities, &
                              find_namelist_in_file, check_namelist_read, &
                              open_file, close_file, nc_check
-use time_manager_mod, only : time_type, print_time, print_date
+use time_manager_mod, only : time_type, print_time, print_date, set_date, &
+                             set_calendar_type, operator(-), get_time
 
 use netcdf
 implicit none
@@ -51,10 +52,11 @@ namelist /openggcm_to_netcdf_nml/ openggcm_to_netcdf_output_file, &
 !----------------------------------------------------------------------
 
 integer               :: io, iunit
-type(time_type)       :: model_time
+type(time_type)       :: model_time, model_time_base, model_time_offset
 real(r4), allocatable :: statevector(:,:)
 real(r8), allocatable :: latitude(:)
 real(r8), allocatable :: longitude(:)
+real(digits12) :: time_offset_seconds
 
 integer :: iyear, imonth, iday, ihour, iminute, isecond
 integer :: ilat, ilon
@@ -62,6 +64,7 @@ integer :: nthe, nphi
 real(r8) :: dlat, dlon
 
 integer :: ncid, NtheDimID, NphiDimID, VarID, LonVarID, LatVarID
+integer :: TimeVarID
 
 !----------------------------------------------------------------------
 
@@ -80,21 +83,15 @@ write(*,'(''openggcm_to_netcdf:converting openggcm restart file '',A, &
       &'' to DART file '',A)') &
        trim(openggcm_to_netcdf_input_file), trim(openggcm_to_netcdf_output_file)
 
-
 iunit = open_file(openggcm_to_netcdf_input_file, form='unformatted',action='read')
-!read(iunit) iyear, imonth, iday, ihour, iminute, isecond
 read(iunit) nphi, nthe
+read(iunit) iyear, imonth, iday, ihour, iminute, isecond
 allocate(statevector(nphi,nthe))
 allocate(latitude(nthe), longitude(nphi))
 read(iunit) statevector
 call close_file(iunit)
 
-iyear   = 2004
-imonth  = 5
-iday    = 8
-ihour   = 9
-iminute = 0
-isecond = 0
+iyear = iyear + 1900
 
 dlat = 180.0_r8/real(nthe-1,r8)
 do ilat = 1,nthe  ! NORTH-to-SOUTH, NORTH is magnetic latitude 0.0
@@ -106,8 +103,20 @@ do ilon = 1,nphi  ! NORTH-to-SOUTH, NORTH is magnetic latitude 0.0
    longitude(ilon) = dlon*real(ilon-1,r8)
 enddo
 
+write(*,*)'TJH iyear, imonth, iday, ihour, iminute, isecond', iyear, imonth, iday, ihour, iminute, isecond
 write(*,*)'TJH nphi, nthe is ',nphi, nthe
 write(*,*)'file size should be ',4 + 4+4 + 4 + 4 + nphi*nthe*4 + 4
+
+call set_calendar_type('Gregorian')
+model_time      = set_date(iyear, imonth, iday, ihour, iminute, isecond)
+model_time_base = set_date(1900, 1, 1, 0, 0, 0)
+model_time_offset = model_time - model_time_base
+call get_time(model_time_offset, isecond, iday)
+
+time_offset_seconds = real(iday,digits12) + real(isecond,digits12)/86400.0_digits12
+
+call print_date(model_time, str='openggcm_to_netcdf:openggcm  model date')
+call print_time(model_time, str='openggcm_to_netcdf:DART model time')
 
 call nc_check(nf90_create(openggcm_to_netcdf_output_file, NF90_CLOBBER, ncid),'openggcm_to_netcdf')
 
@@ -115,6 +124,10 @@ call nc_check(nf90_create(openggcm_to_netcdf_output_file, NF90_CLOBBER, ncid),'o
 
 call nc_check(nf90_def_dim(ncid, 'nphi', nphi, NphiDimID),'openggcm_to_netcdf')
 call nc_check(nf90_def_dim(ncid, 'nthe', nthe, NtheDimID),'openggcm_to_netcdf')
+
+call nc_check(nf90_def_var(ncid, 'time', nf90_double, TimeVarID),'openggcm_to_netcdf')
+call nc_check(nf90_put_att(ncid, TimeVarID, 'units', 'days since 1900-01-01'), &
+                  'put_att time:units: openggcm_to_netcdf')
 
 call nc_check(nf90_def_var(ncid, 'nphi', nf90_real, (/ NphiDimID /), LonVarID),'openggcm_to_netcdf')
 call nc_check(nf90_put_att(ncid, LonVarID, 'short_name', 'geomagnetic longitude'), &
@@ -135,14 +148,13 @@ call nc_check(nf90_put_att(ncid, VarID, 'units', 'volts'), &
 
 call nc_check(nf90_enddef(ncid),'openggcm_to_netcdf')
 
-call nc_check(nf90_put_var(ncid, LonVarID,   longitude),'openggcm_to_netcdf')
-call nc_check(nf90_put_var(ncid, LatVarID,    latitude),'openggcm_to_netcdf')
-call nc_check(nf90_put_var(ncid,    VarID, statevector),'openggcm_to_netcdf')
+call nc_check(nf90_put_var(ncid,  LonVarID,           longitude),'openggcm_to_netcdf')
+call nc_check(nf90_put_var(ncid,  LatVarID,            latitude),'openggcm_to_netcdf')
+call nc_check(nf90_put_var(ncid,     VarID,         statevector),'openggcm_to_netcdf')
+call nc_check(nf90_put_var(ncid, TimeVarID, time_offset_seconds),'openggcm_to_netcdf')
 
 call nc_check(nf90_close(ncid),'openggcm_to_netcdf')
 
-!call print_date(model_time, str='openggcm_to_netcdf:openggcm  model date')
-!call print_time(model_time, str='openggcm_to_netcdf:DART model time')
 call finalize_utilities('openggcm_to_netcdf')
 
 end program openggcm_to_netcdf
