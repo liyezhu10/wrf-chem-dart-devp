b3d2227ec646da51d92aa81f8e97ae1c2d946dce
Tim Hoar <thoar@ucar.edu>
2016-02-25 23:48:56 +0000

    Removing unused files. plot_grid can read the netCDF file produced by openggcm.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/rma_openggcm@9909 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 12aea7c5e0c0e493fff186227ae3b9ed4a66d6dd

--- models/openggcm/matlab/plot_grid.m
+++ models/openggcm/matlab/plot_grid.m
@@ -1,7 +1,7 @@
 function plot_grid(fname)
 %% plot_grid ... plots the ULAT,ULON and TLAT,TLON variables from a netcdf file.
 % 
-% fname = 'h.A1.10.nc';
+% fname = 'DATA.ionos2.nc';
 % plot_grid(fname)
 %
 
@@ -11,99 +11,28 @@ function plot_grid(fname)
 %
 % DART $Id$
 
-ulat = nc_varget(fname,'ULAT') * 180/pi;;
-ulon = nc_varget(fname,'ULON') * 180/pi;;
-tlat = nc_varget(fname,'TLAT') * 180/pi;;
-tlon = nc_varget(fname,'TLON') * 180/pi;;
+cg_lon     = nc_varget(fname,'cg_lon');
+cg_lat     = nc_varget(fname,'cg_lat');
+[cgx, cgy] = meshgrid(cg_lon, cg_lat);
 
-Ulat = ulat(:);
-Ulon = ulon(:);
-Tlat = tlat(:);
-Tlon = tlon(:);
+ig_lon     = nc_varget(fname,'ig_lon');
+ig_lat     = nc_varget(fname,'ig_lat');
+[igx, igy] = meshgrid(ig_lon, ig_lat);
 
-inds = find(tlon <= 0);
-tlon(inds) = tlon(inds) + 360.0;
-inds = find(ulon <= 0);
-ulon(inds) = ulon(inds) + 360.0;
-
-np = 5;
-nx = size(ulat,1);
-ny = size(ulat,2);
+geo_lon = nc_varget(fname,'geo_lon');
+geo_lat = nc_varget(fname,'geo_lat');
 
 figure(1); clf; orient landscape
-   plot(Ulon,Ulat,'ko',Tlon,Tlat,'rx')
-   legend('U,V Grid','S,T Grid')
-   legend boxoff
 
+h1 = plot(cgx,cgy,'ko'); hold on;
+h2 = plot(igx,igy,'rx');
+h3 = plot(geo_lon,geo_lat,'gd');
+% legend([h1,h2,h3],'geographic','magnetic','mag2geo')
+title({'geographic is black o','magnetic is red x', 'mag2geo is green d'})
+xlabel('longitude')
+ylabel('latitude')
 
-figure(2); clf; orient landscape
-
-   i1 =  1;
-   iN = np;
-   j1 = 1;
-   jN = np;
-
-   myplot(i1,iN,j1,jN,ulon,ulat,tlon,tlat)
-
-figure(3); clf; orient landscape
-
-   i1 =  1;
-   iN = np;
-   j1 = ny-np+1;
-   jN = ny;
-
-   myplot(i1,iN,j1,jN,ulon,ulat,tlon,tlat)
-
-figure(4); clf; orient landscape
-
-   i1 = nx-np+1;
-   iN = nx;
-   j1 = ny-np+1;
-   jN = ny;
-
-   myplot(i1,iN,j1,jN,ulon,ulat,tlon,tlat)
-
-figure(5); clf; orient landscape
-
-   i1 = nx-np+1;
-   iN = nx;
-   j1 = 1;
-   jN = np;
-
-   myplot(i1,iN,j1,jN,ulon,ulat,tlon,tlat)
-
-
-function myplot(i1,iN,j1,jN,ulon,ulat,tlon,tlat)
-
-   h = plot(ulon(i1:iN,j1:jN), ulat(i1:iN,j1:jN), 'ko', ...
-            tlon(i1:iN,j1:jN), tlat(i1:iN,j1:jN), 'rx');
-   set(h,'MarkerSize',1)
-
-   for i = i1:iN
-   for j = j1:jN
-   %  h = text(ulon(i,j),ulat(i,j), sprintf('%d,%d',i,j));
-      str   = sprintf('''%d,%d''',i,j);
-      mystr = sprintf('h1 = text(%f,%f,%s);',ulon(i,j),ulat(i,j),str);
-      eval(mystr)
-      set(h1,'HorizontalAlignment','center', ...
-            'VerticalAlignment','middle', ...
-            'FontSize',10,'Color','k')
-
-      mystr = sprintf('h2 = text(%f,%f,%s);',tlon(i,j),tlat(i,j),str);
-      eval(mystr)
-      set(h2,'HorizontalAlignment','center', ...
-            'VerticalAlignment','middle', ...
-            'FontSize',10,'Color','r')
-   end
-   end
-   title('POP grid layout')
-   xlabel('U,V grid in black; T,S grid in red')
-   set(gca,'box','off')
-
-%  set(h,'Visible','off'); % Get the domain right.
-%   h = [h1 h2];
-%  legend('U,V Grid','S,T Grid')
-%  legend boxoff
+hold off;
 
 % <next few lines under version control, do not edit>
 % $URL$
