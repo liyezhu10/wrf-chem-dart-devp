00813031238d3e63af77482fc7bbfbf6ae9a9553
Tim Hoar <thoar@ucar.edu>
2016-02-26 22:41:33 +0000

    Confirming the problem with the data at one particular longitude.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/rma_openggcm@9929 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 3b1ca7a2c3db7125ef58cca5716676fe0f5e8c0f

--- models/openggcm/matlab/plot_interp_diffs.m
+++ models/openggcm/matlab/plot_interp_diffs.m
@@ -24,34 +24,43 @@ byte = fread(ggcm,1,'int32');
 stat = fclose(ggcm);
 
 datmat = reshape(gdat,dim1,dim2,dim3);
-size(datmat)
-
-
-figure(1)
-
-level = 1;
-imagesc(squeeze(datmat(:,:,level)));
-
-h= title({sprintf('%s level %d',fname, level), sprintf('%d -x- %d -x- %d',dim1,dim2,dim3)});
-set(gca,'YDir','normal');
-ylabel(sprintf('dimension %d (length=%d)',1,dim1))
-xlabel(sprintf('dimension %d (length=%d)',2,dim2))
-axis image
-wysiwyg
-set(gca,'FontSize',20)
-colorbar('southoutside')
-
-figure(2)
 
 f2 = '../data/DATA.ionos2.nc';
 oplus = nc_varget(f2,'oplus');
 
+size(datmat)
 size(oplus)
 
-imagesc(squeeze(oplus(:,:,level)));
-set(gca,'Ydir','normal')
-axis image
-colorbar('southoutside')
+for ilon = 1:dim3
+
+   fprintf('longitude %d\n',ilon)
+
+   figure(1); orient tall;
+   subplot(2,1,1)
+   level = 1;
+   imagesc(squeeze(datmat(:,:,ilon)));
+   
+   h= title({sprintf('%s longitude %d',fname, ilon), sprintf('%d -x- %d -x- %d',dim1,dim2,dim3)});
+   set(gca,'YDir','normal');
+   ylabel(sprintf('dimension %d (length=%d)',1,dim1))
+   xlabel(sprintf('dimension %d (length=%d)',2,dim2))
+   axis image
+   wysiwyg
+   set(gca,'FontSize',20)
+   colorbar('eastoutside')
+   
+   subplot(2,1,2)
+   imagesc(squeeze(oplus(ilon,:,:))');
+   set(gca,'Ydir','normal')
+   axis image
+   set(gca,'FontSize',20)
+   colorbar('eastoutside')
+
+   disp('Pausing, hit any key to continue ...')
+   pause
+
+end
+
 
 
 % <next few lines under version control, do not edit>
