297940a399c079815bc4c9b3dd1fdf256a4340cc
Tim Hoar <thoar@ucar.edu>
2016-03-03 16:43:55 +0000

    Combined openggcm_netcdf_write_routines.f and openggcm_netcdf_read_routines.f into one file as per Jimmy's request. Will test and rename in a separate commit.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/rma_openggcm@9946 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 018d2b57f0c2e102e52ac7c9edb72b3adb1d9e05

--- models/openggcm/openggcm_netcdf_write_routines.f
+++ models/openggcm/openggcm_netcdf_write_routines.f
@@ -4,7 +4,7 @@
 !
 ! $Id$
 
-      module netcdf_write_mod
+      module netcdf_utilities
 
       implicit none
       private
@@ -13,6 +13,7 @@
      &          wr_netcdf_ctim_grid,
      &          wr_netcdf_interface_grid,
      &          wr_netcdf
+     &          rd_netcdf
 
       interface wr_netcdf
          module procedure wr_netcdf_r4_1D
@@ -22,6 +23,15 @@
          module procedure wr_netcdf_r8_2D
          module procedure wr_netcdf_r8_3D
       end interface wr_netcdf
+
+      interface rd_netcdf
+         module procedure rd_netcdf_r4_1D
+         module procedure rd_netcdf_r4_2D
+         module procedure rd_netcdf_r4_3D
+         module procedure rd_netcdf_r8_1D
+         module procedure rd_netcdf_r8_2D
+         module procedure rd_netcdf_r8_3D
+      end interface rd_netcdf
 
       contains
 
@@ -663,7 +673,253 @@
       end subroutine wr_netcdf_r8_3D
 
 !=======================================================================
-! .... remove everything below here when putting into openggcm
+! Start of the read routines
+!=======================================================================
+
+      subroutine rd_netcdf_r4_1D(ncid, tensorname, dim1, tensor)
+
+      use netcdf
+      implicit none
+
+      integer,          intent(in)  :: ncid
+      character(len=*), intent(in)  :: tensorname
+      integer,          intent(in)  :: dim1
+      real*4,           intent(out) :: tensor(dim1)
+
+      integer :: dim1ID, dim2ID, dim3ID, VarID
+      integer :: ndim1, ndim2, ndim3
+      integer :: io
+
+      io = nf90_inq_varid(ncid, tensorname, VarID)
+      call nc_check(io, 'rd_netcdf_r4_1D', 'inq_varid: '//trim(tensorname))
+
+      call get_dimensions(ncid, VarID, tensorname, dim1ID, ndim1, dim2ID, ndim2, dim3ID, ndim3)
+
+      if (dim1 /= ndim1) then
+         write(*,*)'ERROR :: rd_netcdf_r4_1D: failed dimensions '//trim(tensorname),dim1
+         write(*,*)'ERROR :: rd_netcdf_r4_1D: expected dimensions ',ndim1
+         call death()
+      endif
+
+      io = nf90_get_var(ncid, VarID, tensor)
+      call nc_check(io, 'rd_netcdf_r4_1D', 'get_var: '//trim(tensorname))
+
+      end subroutine rd_netcdf_r4_1D
+
+!=======================================================================
+
+      subroutine rd_netcdf_r8_1D(ncid, tensorname, dim1, tensor)
+
+      use netcdf
+      implicit none
+
+      integer,          intent(in)  :: ncid
+      character(len=*), intent(in)  :: tensorname
+      integer,          intent(in)  :: dim1
+      real*8,           intent(out) :: tensor(dim1)
+
+      integer :: dim1ID, dim2ID, dim3ID, VarID
+      integer :: ndim1, ndim2, ndim3
+      integer :: io
+
+      io = nf90_inq_varid(ncid, tensorname, VarID)
+      call nc_check(io, 'rd_netcdf_r8_1D', 'inq_varid: '//trim(tensorname))
+
+      call get_dimensions(ncid, VarID, tensorname, dim1ID, ndim1, dim2ID, ndim2, dim3ID, ndim3)
+
+      if (dim1 /= ndim1) then
+         write(*,*)'ERROR :: rd_netcdf_r8_1D: failed dimensions '//trim(tensorname),dim1
+         write(*,*)'ERROR :: rd_netcdf_r8_1D: expected dimensions ',ndim1
+         call death()
+      endif
+
+      io = nf90_get_var(ncid, VarID, tensor)
+      call nc_check(io, 'rd_netcdf_r8_1D', 'get_var: '//trim(tensorname))
+
+      end subroutine rd_netcdf_r8_1D
+
+!=======================================================================
+
+      subroutine rd_netcdf_r4_2D(ncid, tensorname, dim1, dim2, tensor)
+
+      use netcdf
+      implicit none
+
+      integer,          intent(in)  :: ncid
+      character(len=*), intent(in)  :: tensorname
+      integer,          intent(in)  :: dim1, dim2
+      real*4,           intent(out) :: tensor(dim1,dim2)
+
+      integer :: dim1ID, dim2ID, dim3ID, VarID
+      integer :: ndim1, ndim2, ndim3
+      integer :: io
+
+      io = nf90_inq_varid(ncid, tensorname, VarID)
+      call nc_check(io, 'rd_netcdf_r4_2D', 'inq_varid: '//trim(tensorname))
+
+      call get_dimensions(ncid, VarID, tensorname, dim1ID, ndim1, dim2ID, ndim2, dim3ID, ndim3)
+
+      if ((dim1 /= ndim1) .or. (dim2 /= ndim2)) then
+         write(*,*)'ERROR :: rd_netcdf_r4_2D: failed dimensions '//trim(tensorname),dim1,dim2
+         write(*,*)'ERROR :: rd_netcdf_r4_2D: expected dimensions ',ndim1,ndim2
+         call death()
+      endif
+
+      io = nf90_get_var(ncid, VarID, tensor)
+      call nc_check(io, 'rd_netcdf_r4_2D', 'get_var: '//trim(tensorname))
+
+      end subroutine rd_netcdf_r4_2D
+
+!=======================================================================
+
+      subroutine rd_netcdf_r8_2D(ncid, tensorname, dim1, dim2, tensor)
+
+      use netcdf
+      implicit none
+
+      integer,          intent(in)  :: ncid
+      character(len=*), intent(in)  :: tensorname
+      integer,          intent(in)  :: dim1, dim2
+      real*8,           intent(out) :: tensor(dim1,dim2)
+
+      integer :: dim1ID, dim2ID, dim3ID, VarID
+      integer :: ndim1, ndim2, ndim3
+      integer :: io
+
+      io = nf90_inq_varid(ncid, tensorname, VarID)
+      call nc_check(io, 'rd_netcdf_r8_2D', 'inq_varid: '//trim(tensorname))
+
+      call get_dimensions(ncid, VarID, tensorname, dim1ID, ndim1, dim2ID, ndim2, dim3ID, ndim3)
+
+      if ((dim1 /= ndim1) .or. (dim2 /= ndim2)) then
+         write(*,*)'ERROR :: rd_netcdf_r8_2D: failed dimensions '//trim(tensorname),dim1,dim2
+         write(*,*)'ERROR :: rd_netcdf_r8_2D: expected dimensions ',ndim1,ndim2
+         call death()
+      endif
+
+      io = nf90_get_var(ncid, VarID, tensor)
+      call nc_check(io, 'rd_netcdf_r8_2D', 'get_var: '//trim(tensorname))
+
+      end subroutine rd_netcdf_r8_2D
+
+!=======================================================================
+
+      subroutine rd_netcdf_r4_3D(ncid, tensorname, dim1, dim2, dim3, tensor)
+
+      use netcdf
+      implicit none
+
+      integer,          intent(in)  :: ncid
+      character(len=*), intent(in)  :: tensorname
+      integer,          intent(in)  :: dim1, dim2, dim3
+      real*4,           intent(out) :: tensor(dim1,dim2,dim3)
+
+      integer :: dim1ID, dim2ID, dim3ID, VarID
+      integer :: ndim1, ndim2, ndim3
+      integer :: io
+
+      io = nf90_inq_varid(ncid, tensorname, VarID)
+      call nc_check(io, 'rd_netcdf_r4_3D', 'inq_varid: '//trim(tensorname))
+
+      call get_dimensions(ncid, VarID, tensorname, dim1ID, ndim1, dim2ID, ndim2, dim3ID, ndim3)
+
+      if ((dim1 /= ndim1) .or. (dim2 /= ndim2) .or. (dim3 /= ndim3)) then
+         write(*,*)'ERROR :: rd_netcdf_r4_3D: failed dimensions '//trim(tensorname),dim1,dim2,dim3
+         write(*,*)'ERROR :: rd_netcdf_r4_3D: expected dimensions ',ndim1,ndim2,ndim3
+         call death()
+      endif
+
+      io = nf90_get_var(ncid, VarID, tensor)
+      call nc_check(io, 'rd_netcdf_r4_3D', 'get_var: '//trim(tensorname))
+
+      end subroutine rd_netcdf_r4_3D
+
+!=======================================================================
+
+      subroutine rd_netcdf_r8_3D(ncid, tensorname, dim1, dim2, dim3, tensor)
+
+      use netcdf
+      implicit none
+
+      integer,          intent(in)  :: ncid
+      character(len=*), intent(in)  :: tensorname
+      integer,          intent(in)  :: dim1, dim2, dim3
+      real*8,           intent(out) :: tensor(dim1,dim2,dim3)
+
+      integer :: dim1ID, dim2ID, dim3ID, VarID
+      integer :: ndim1, ndim2, ndim3
+      integer :: io
+
+      io = nf90_inq_varid(ncid, tensorname, VarID)
+      call nc_check(io, 'rd_netcdf_r8_3D', 'inq_varid: '//trim(tensorname))
+
+      call get_dimensions(ncid, VarID, tensorname, dim1ID, ndim1, dim2ID, ndim2, dim3ID, ndim3)
+
+      if ((dim1 /= ndim1) .or. (dim2 /= ndim2) .or. (dim3 /= ndim3)) then
+         write(*,*)'ERROR :: rd_netcdf_r8_3D: failed dimensions '//trim(tensorname),dim1,dim2,dim3
+         write(*,*)'ERROR :: rd_netcdf_r8_3D: expected dimensions ',ndim1,ndim2,ndim3
+         call death()
+      endif
+
+      io = nf90_get_var(ncid, VarID, tensor)
+      call nc_check(io, 'rd_netcdf_r8_3D', 'get_var: '//trim(tensorname))
+
+      end subroutine rd_netcdf_r8_3D
+
+!=======================================================================
+
+      subroutine get_dimensions(ncid, VarID, variablename, dim1ID, ndim1, dim2ID, ndim2, dim3ID, ndim3)
+
+      use netcdf
+      implicit none
+
+      integer,          intent(in)  :: ncid
+      integer,          intent(in)  :: VarID
+      character(len=*), intent(in)  :: variablename ! for output messages
+      integer,          intent(out) :: dim1ID, ndim1
+      integer,          intent(out) :: dim2ID, ndim2
+      integer,          intent(out) :: dim3ID, ndim3
+
+      integer, dimension(NF90_MAX_VAR_DIMS) :: dimIDs
+      integer, dimension(NF90_MAX_VAR_DIMS) :: dimlens
+
+      integer :: io
+      integer :: numdims, i
+
+      character(len=512) :: string1
+
+      ! set defaults
+      dimlens(:) = -1
+      dim1ID     = -1
+      dim2ID     = -1
+      dim3ID     = -1
+
+      io = nf90_inquire_variable(ncid, VarID, dimids=dimIDs, ndims=numdims)
+      call nc_check(io, 'get_dimensions', 'inquire variable '//trim(variablename))
+
+      if ((numdims > 3) .or. (numdims < 1)) then
+         write(*,*)'ERROR: unexpected number of dimensions for '//trim(variablename)
+         write(*,*)'ERROR: expected 1 or 2 or 3, got ',numdims
+         call death()
+      endif
+
+      do i = 1,numdims
+         write(string1,*)trim(variablename)//' dimension ',i
+         io = nf90_inquire_dimension(ncid, dimIDs(i), len=dimlens(i))
+         call nc_check(io, 'get_dimensions ', trim(string1))
+      enddo
+
+      ndim1  = dimlens(1)
+      dim1ID =  dimids(1)
+
+      ndim2  = dimlens(2)
+      dim2ID =  dimids(2)
+
+      ndim3  = dimlens(3)
+      dim3ID =  dimids(3)
+
+      end subroutine get_dimensions
+
 !=======================================================================
 
       subroutine nc_check(istatus, subr_name, context)
@@ -688,7 +944,9 @@
 
       end subroutine nc_check
 
-!===================================================================
+!=======================================================================
+! .... remove everything below here when putting into openggcm
+!=======================================================================
 
       subroutine death()
 
@@ -698,7 +956,7 @@
 
 !===================================================================
 
-      end module netcdf_write_mod
+      end module netcdf_utilities
 
 ! <next few lines under version control, do not edit>
 ! $URL$
