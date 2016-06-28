37108621e52ee2906790f634d9fc6aacd1aec540
Johnny Hendricks <Johnny>
2016-06-28 17:12:52 +0000

    T&J dist mem accross groups and created global_static_data, took out pointers, took out duplicates, have each thread reading stacic data but getting only its distributed portion
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/rma_pop_static_data@10412 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 6edec3531513942df99eb80aa164b170ec21a7bc

--- models/POP/distributed_static_data_mod.f90
+++ models/POP/distributed_static_data_mod.f90
@@ -24,11 +24,9 @@ public :: create_groups, build_my_group, initialize_static_data_space, &
           distribute_static_data
 
 ! grid window
-real(r8), allocatable :: static_data(:) !< local static data 
+real(r8), allocatable :: global_static_data(:) !< local static data 
 
 integer  :: sd_window
-real(r8) :: duplicate_static_data(*)
-pointer(aa, duplicate_static_data)
 
 integer :: group_size = 4 !< should be namelist option
 integer :: group_handle   !< mpi_comm_world group
@@ -38,11 +36,11 @@ integer :: local_rank     !< rank within group
 integer, allocatable :: group_members(:)
 
 ! pntecdf variables
-integer(KIND=MPI_OFFSET_KIND) :: x_length, y_length, num_static_arrays
+integer(KIND=MPI_OFFSET_KIND) :: x_start, x_length, y_length, num_static_arrays
 integer(KIND=MPI_OFFSET_KIND) :: start(4)
 integer(KIND=MPI_OFFSET_KIND) :: count(4)
 integer(KIND=MPI_OFFSET_KIND) :: stride(4)
-integer(KIND=MPI_OFFSET_KIND) :: my_num_vals !< splitting up by we
+integer(KIND=MPI_OFFSET_KIND) :: my_num_x_vals !< splitting up by we
 
 integer :: array_number = 0 ! starting index for array
 
@@ -122,25 +120,9 @@ integer ii, jj, kk, ierr, sizedouble, count
 integer(KIND=MPI_ADDRESS_KIND) :: window_size
 
 call mpi_type_size(datasize, sizedouble, ierr) ! datasize comes from mpi_utilities_mod
-window_size = num_static_arrays*my_num_vals*y_length*sizedouble
-aa = malloc(num_static_arrays*my_num_vals*y_length)
-call MPI_ALLOC_MEM(window_size, mpi_info_null, aa, ierr)
+window_size = num_static_arrays*my_num_x_vals*y_length*sizedouble
 
-! can't do array assignment with a cray pointer, so you need to loop
-count = 1
-
-! which order should these be in? (z, x, y) so you can grab a column at a time?
-do jj = 1, num_static_arrays
-   do kk = 1, my_num_vals
-      do ii = 1, y_length
-         duplicate_static_data(count) = static_data(kk)!, jj, ii)
-         !print*, 'count ', count
-         count = count + 1
-      enddo
-   enddo
-enddo
-
-call mpi_win_create(duplicate_static_data,  window_size, sizedouble, MPI_INFO_NULL, mpi_comm_grid, sd_window, ierr)
+call mpi_win_create(global_static_data,  window_size, sizedouble, MPI_INFO_NULL, mpi_comm_grid, sd_window, ierr)
 
 end subroutine create_window
 
@@ -151,7 +133,6 @@ subroutine free_window
 integer :: ierr
 
 call mpi_win_free(sd_window, ierr)
-call MPI_FREE_MEM(duplicate_static_data, ierr)
 
 deallocate(group_members)
 
@@ -170,7 +151,7 @@ integer(KIND=MPI_ADDRESS_KIND)   :: target_disp !< displacement
 integer                          :: ierr
 
 ! caluclate who has the info
-call who_has_grid_info(Static_ID, i, j, owner, target_disp)
+! call who_has_grid_info(Static_ID, i, j, owner, target_disp)
 
 ! grab the info
 call mpi_win_lock(MPI_LOCK_SHARED, owner, 0, sd_window, ierr)
@@ -181,38 +162,38 @@ end function get_static_data
 
 !---------------------------------------------------------
 !> get the owner and location in memory of the grid point
-subroutine who_has_grid_info(Static_ID, i, j, owner, target_disp)
-
-integer,                        intent(in)  :: Static_ID
-integer,                        intent(in)  :: i
-integer,                        intent(in)  :: j
-integer,                        intent(out) :: owner
-integer(KIND=MPI_ADDRESS_KIND), intent(out) :: target_disp !< displacement
-
-integer :: temp
-integer :: local_we      !< local index of my slice of we
-integer :: owner_num_we  !< length of we slab on the owner
-integer :: we
-
-temp = x_length / group_size ! phb is split only in we (west-east)
-
-we = i-1
-owner  = we / temp ! we is i
-
-! last task in the group has the most grid points
-if (owner > group_size - 1) owner = group_size - 1
-
-if (owner < group_size - 1) then
-   owner_num_we = temp
-else
-   owner_num_we = x_length - temp*(group_size - 1)
-endif
-
-! phb is a 3d array. (z,x,y) window
-local_we = i - temp*owner  
-target_disp = (j - 1)*y_length*owner_num_we + (local_we- 1)*y_length
-
-end subroutine who_has_grid_info
+! subroutine who_has_grid_info(Static_ID, i, j, owner, target_disp)
+! 
+! integer,                        intent(in)  :: Static_ID
+! integer,                        intent(in)  :: i
+! integer,                        intent(in)  :: j
+! integer,                        intent(out) :: owner
+! integer(KIND=MPI_ADDRESS_KIND), intent(out) :: target_disp !< displacement
+! 
+! integer :: temp
+! integer :: local_we      !< local index of my slice of we
+! integer :: owner_num_we  !< length of we slab on the owner
+! integer :: we
+! 
+! temp = x_length / group_size ! phb is split only in we (west-east)
+! 
+! we = i-1
+! owner  = we / temp ! we is i
+! 
+! ! last task in the group has the most grid points
+! if (owner > group_size - 1) owner = group_size - 1
+! 
+! if (owner < group_size - 1) then
+!    owner_num_we = temp
+! else
+!    owner_num_we = x_length - temp*(group_size - 1)
+! endif
+! 
+! ! phb is a 3d array. (z,x,y) window
+! local_we = i - temp*owner  
+! target_disp = (j - 1)*y_length*owner_num_we + (local_we- 1)*y_length
+! 
+! end subroutine who_has_grid_info
 
 !---------------------------------------------------------
 !> Initalize local static data for each processor
@@ -232,106 +213,32 @@ x_length          = NX
 y_length          = NY
 
 ! split up in x dimension, could also split in y?
-bin_size = x_length / group_size
-if (local_rank < group_size - 1) then
-   my_num_vals = bin_size
-   !write(*,'(''first rank['',I2,''] my_num_vals * NY = '',I4,'' * '',I4,'' = '',I7)') &
-   !       my_task_id(), my_num_vals, NY, my_num_vals*NY
-else ! last processor in group
-   my_num_vals = x_length - bin_size*(group_size - 1)
-   !write(*,'(''last  rank['',I2,''] my_num_vals * NY = '',I4,'' * '',I4,'' = '',I7)') &
-   !       my_task_id(), my_num_vals, NY, my_num_vals*NY
+my_num_x_vals = x_length / group_size
+x_start       = local_rank*my_num_x_vals
+if (local_rank == group_size - 1) then
+   my_num_x_vals = my_num_x_vals + mod(x_length,group_size)
+   !write(*,'(''last  rank['',I2,''] my_num_x_vals * y_length = '',I4,'' * '',I4,'' = '',I7)') &
+   !       my_task_id(), my_num_x_vals, y_length, my_num_x_vals*y_length
 endif
 
-allocate(static_data(num_static_arrays*my_num_vals*y_length)) ! local static data
+allocate(global_static_data(num_static_arrays*my_num_x_vals*y_length)) ! local static data
 
 end subroutine initialize_static_data_space
 
 !---------------------------------------------------------
 !> Distribute the static data to the appropriate processors
-function distribute_static_data(source, static_array, NX, NY) result(static_id)
+function distribute_static_data(local_static_data) result(static_id)
 
-integer,  intent(in)    :: source
-real(r8), intent(inout) :: static_array(NX,NY)
-integer,  intent(in)    :: NX
-integer,  intent(in)    :: NY
+real(r8), intent(inout) :: local_static_data(x_length,y_length)
 integer :: static_id
+integer :: istart, iend
 
-integer  :: bin_size, my_num_vals
-!real(r8) :: linear_array(NX*NY)
-real(r8), allocatable :: var_block(:)
-
-integer :: my_pe, my_rank, i, j
-integer :: recv_pe
 ! block data
 static_id = array_number + 1
+istart = (static_id-1)*x_start*y_length+1
+iend   = istart+my_num_x_vals*y_length
 
-! post receives
-do my_rank = 0,group_size-1
-      bin_size = NX / group_size
-      if (my_rank < group_size-1) then
-         my_num_vals = bin_size
-      else ! last processor in group
-         my_num_vals = NX - bin_size*(group_size-1)
-      endif
-      if(local_rank == my_rank) then
-         write(*,*) 'my rank ', my_task_id(), 'local rank',local_rank,'recieving from ',source
-         call receive_from(source, static_data((static_id-1)*my_num_vals*NY+1:static_id*my_num_vals*NY))
-      endif
-enddo
-
-
-if(my_task_id() == source) then
-
-   do my_rank = 0, group_size-1
-
-      bin_size = NX / group_size
-      if (my_rank < group_size-1) then
-         my_num_vals = bin_size
-         !write(*,'(''first rank['',I2,''] my_num_vals * NY = '',I4,'' * '',I4,'' = '',I7)') &
-         !       my_rank, my_num_vals, NY, my_num_vals*NY
-      else ! last processor in group
-         my_num_vals = NX - bin_size*(group_size-1)
-         !write(*,'(''last  rank['',I2,''] my_num_vals * NY = '',I4,'' * '',I4,'' = '',I7)') &
-         !       my_rank, my_num_vals, NY, my_num_vals*NY
-      endif
-
-      !write(*,*) 'my_num_vals*NY =  ', my_rank, my_num_vals*NY
-
-      allocate(var_block(my_num_vals*NY))
-         !write(*,*) 'static_array ', static_array
-         do j = 1, my_num_vals
-            do i = 1, NY
-                var_block(i+(j-1)*NY) = static_array(i,j+(my_rank)*group_size)
-            enddo
-         enddo
-         if (my_task_id() == source) then
-            write(*,*) 'source',source,'sending to ', my_rank
-            call send_to(group_members(my_rank+1), var_block)
-         else
-            ! directly copy into source task
-            print*,'range', (static_id-1)*my_num_vals*NY+1,static_id*my_num_vals*NY
-            
-            static_data((static_id-1)*my_num_vals*NY+1:static_id*my_num_vals*NY) = var_block(:)
-         endif
-         ! write(*,*) 'var_block ', my_rank, var_block    
-      deallocate(var_block)
-         !linear_array = reshape(static_data, (/NX*NY/))
-         !write(*,*) 'SIZE lin array : ', size(linear_array)
-         !write(*,*) 'array number   : ', array_number
-         !write(*,*) 'lin_array(1:10): ', linear_array(1:5)
-   enddo
-endif
-
-write(*,*) 'my_rank, ',my_task_id(),  static_data
-write(*,*) ' '
-
-! 
-! var_block(:) = 
-! 
-! if (my_task_id() == source) then
-!    call send_to(dest_id, srcarray, time, label)
-! else
+global_static_data(:) = local_static_data(istart:iend) 
 
 
 end function distribute_static_data
