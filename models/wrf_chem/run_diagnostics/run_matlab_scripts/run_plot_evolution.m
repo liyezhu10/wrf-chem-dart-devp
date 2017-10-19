595f199a01dfdd3d53ef33876099a190a77d4407
Arthur Mizzi <Arthur>
2017-10-19 18:28:47 +0000

    Cleaning up the WRF-Chem/DART repository
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/mizzi@12009 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 0e8d5ee84c9c53036a4d25f9742bb2d2d022b5e1

--- models/wrf_chem/run_diagnostics/run_matlab_scripts/run_plot_evolution.m
+++ models/wrf_chem/run_diagnostics/run_matlab_scripts/run_plot_evolution.m
@@ -1,16 +1,14 @@
 %
 fname         = '/glade2/scratch2/mizzi/DART_OBS_DIAG/real_FRAPPE_RETR_MOP_CO/obs_diag_output.nc';
 %
-npar=1;
-%copystring    = {'rmse'};
-%copystring    = {'totalspread'};
-copystring    = {'spread'};
+npar=2;
+copystring    = {'rmse','totalspread'};
 %
-nvar=1;
-varname      = {'MOPITT_CO_RETRIEVAL'};
+nvar=3;
+varname      = {'MOPITT_CO_RETRIEVAL','IASI_O3_RETRIEVAL','AIRNOW_O3'};
 for ipar=1:npar
    for ivar=1:nvar
 %     plot = plot_evolution(fname,copystring{ipar},'varname',varname{ivar},'range',[lbnd,ubnd]);
-      plot = plot_evolution(fname,copystring{ipar},'varname',varname{ivar});
+      plot = plot_evolution(fname,copystring{ipar},'obsname',varname{ivar});
    end
 end
