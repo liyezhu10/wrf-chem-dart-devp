3f8550a28d9f16e868ee8cc709cdbe2bc46a3ffe
Tim Hoar <thoar@ucar.edu>
2017-12-14 21:00:56 +0000

    Basically renaming the EASE-Grid converter to NSIDC, since the National Snow and Ice Data Center is where the data come from.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/cesm_clm@12202 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: a1630868d16aed35c5bad4509ad76146f1d65ffd

--- observations/obs_converters/NSIDC/readhdf5.f90
+++ observations/obs_converters/NSIDC/readhdf5.f90
@@ -2,12 +2,11 @@
 program readhdf5
 
 use HDF5
-use H5LT
 
 implicit none
 
-character(len=*), parameter   :: filename = 'SMAP_L2_SM_P_02522_D_20150723T002821_R12170_001.h5'
-character(len=*), parameter   :: dset_name = '/Soil_Moisture_Retrieval_Data/longitude'
+character(len=256) :: filename = 'data/SMAP_L2_SM_P_02522_D_20150723T002821_R12170_001.h5'
+
 integer                       :: hdferr, rank
 integer(HID_T)                :: file_id, dset_id, dspace_id
 integer(HSIZE_T), allocatable :: dims(:), maxdims(:)
@@ -25,7 +24,7 @@ call h5fopen_f(filename, H5F_ACC_RDONLY_F, file_id, hdferr)
 write(*,*)'h5fopen_f error, file_id ',hdferr,file_id
 
 ! open the dataset
-call h5dopen_f(file_id,dset_name,dset_id, hdferr)
+call h5dopen_f(file_id,'/Soil_Moisture_Retrieval_Data/longitude',dset_id, hdferr)
 write(*,*)'h5dopen_f error, dset_id is ',hdferr,dset_id
 
 ! open the dataspace
@@ -49,29 +48,11 @@ endif
 !allocate input data to the dimensions
 allocate(longitude(dims(1)))
 
-!TJH ! read the data using the dataset ID
-!TJH call h5dread_f(dset_id, H5T_NATIVE_REAL, longitude, dims, hdferr)
-!TJH write(*,*)'h5dread_f with dset_id error is ',hdferr
-!TJH if (hdferr == 0) write(*,*)longitude(1:10)
+! read the data using the dataset ID
+call h5dread_f(dset_id, H5T_NATIVE_REAL, longitude, dims, hdferr)
+write(*,*)'h5dread_f with dset_id error is ',hdferr
 
-!TJH ! read the data using the dataspace ID
-!TJH call h5dread_f(dspace_id, H5T_NATIVE_REAL, longitude, dims, hdferr)
-!TJH write(*,*)'h5dread_f with dspace_id error is ',hdferr
-!TJH if (hdferr == 0) write(*,*)longitude(1:10)
-
-! Use the H5LT interfaces ... ha ha ha
-
-!TJH  call h5ltread_dataset_f(loc_id, dset_name, type_id, buf, dims, errcode)
-!TJH  integer(HID_T),   intent(IN)    :: loc_id    ! file or group identifier 
-!TJH  character(LEN=*), intent(IN)    :: dset_name ! name of the dataset 
-!TJH  integer(HID_T),   intent(IN)    :: type_id   ! datatype identifier 
-!TJH  integer(HSIZE_T), intent(IN)    :: dims(:)   ! size of the buffer buf 
-!TJH  <TYPE>,           intent(INOUT) :: buf(:)    ! data buffer 
-!TJH  integer :: errcode                           ! error code
-
-call h5ltread_dataset_float_f(file_id, dset_name, longitude, dims, hdferr)
-write(*,*)'h5ltread_dataset_float_f error, dims(:) is ',hdferr, dims
-
+if (hdferr == 0) write(*,*)longitude(1:10)
 
 deallocate(longitude, dims)
 
