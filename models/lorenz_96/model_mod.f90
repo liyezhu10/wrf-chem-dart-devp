049a641e693fd485eabe0e7c045a8277fb275150
Nancy Collins <nancy@ucar.edu>
2017-12-22 15:54:57 +0000

    use the longer names for the netcdf routines.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/recam@12237 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 8f508c0e9c610ac99fecce2c2f13d9a660eaa4c5

--- models/lorenz_96/model_mod.f90
+++ models/lorenz_96/model_mod.f90
@@ -21,8 +21,9 @@ use utilities_mod,         only : register_module, do_nml_file, do_nml_term,    
 use location_io_mod,      only :  nc_write_location_atts, nc_get_location_varids, &
                                   nc_write_location
 
-use netcdf_utilities_mod, only : nc_add_global_attribute, nc_sync, &
-                                 nc_add_global_creation_time, nc_redef, nc_enddef
+use netcdf_utilities_mod, only : nc_add_global_attribute, nc_synchronize_file, &
+                                 nc_add_global_creation_time, nc_begin_define_mode, &
+                                 nc_end_define_mode
 
 use         obs_kind_mod,  only : QTY_STATE_VARIABLE
 
@@ -317,7 +318,7 @@ character(len=128)  :: filename
 
 msize = int(model_size, i4)
 
-call nc_redef(ncid)
+call nc_begin_define_mode(ncid)
 
 call nc_add_global_creation_time(ncid)
 
@@ -330,10 +331,10 @@ call nc_add_global_attribute(ncid, "model_forcing", forcing )
 call nc_add_global_attribute(ncid, "model_delta_t", delta_t )
 
 call nc_write_location_atts(ncid, msize)
-call nc_enddef(ncid)
+call nc_end_define_mode(ncid)
 call nc_write_location(ncid, state_loc, msize)
 
-call nc_sync(ncid)
+call nc_synchronize_file(ncid)
 
 end subroutine nc_write_model_atts
 
