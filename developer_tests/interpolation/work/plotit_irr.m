5e1aa5d4f0ed7b6fb8d24a4a64f095178eb83e50
Nancy Collins <nancy@ucar.edu>
2018-10-08 21:41:12 +0000

    forgot to move the replacement of missing values. it works better if that code comes before the reshape() call.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/roms_interpolation@12897 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: d576d1b3e62e85d709d0f38048f08382adf67c02

--- developer_tests/interpolation/work/plotit_irr.m
+++ developer_tests/interpolation/work/plotit_irr.m
@@ -3,13 +3,16 @@ function plotit_irr(lon, lat, dat, msize)
 nlon = length(lon(1,:));
 nlat = length(lat(:,1));
 
-my_data = reshape(dat, nlon, nlat);
 
 % look for missing r8s
 missing_val = -888888;
 inds = find(my_data==missing_val);
-if (~isempty(inds)), my_data(inds)  = 0.; end
-%if (~isempty(inds)), my_data(inds)  = nan; end
+% choose nan or 0 for plotting, whichever gives
+% the easiest-to-interpret results.
+if (~isempty(inds)), my_data(inds)  = nan; end
+%f (~isempty(inds)), my_data(inds)  = 0.; end
+
+my_data = reshape(dat, nlon, nlat);
 
 scatter(lon, lat, msize, my_data,'filled');
 %plot3(lon, lat, my_data);
