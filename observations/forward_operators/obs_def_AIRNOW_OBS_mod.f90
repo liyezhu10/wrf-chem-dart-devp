ff425cfb67e241f516a2f160a29ac87a078bbf65
Kate Fossell <fossell@ucar.edu>
2019-09-16 14:26:35 -0600

    Add wrf-chem AIRNOW obs_def

---
+++ observations/forward_operators/obs_def_AIRNOW_OBS_mod.f90
@@ -0,0 +1,28 @@
+! DART software - Copyright 2004 - 2013 UCAR. This open source software is
+! provided by UCAR, "as is", without charge, subject to all terms of use at
+! http://www.image.ucar.edu/DAReS/DART/DART_download
+!
+! DART $Id: obs_def_AIRNOW_OBS_mod.f90 11954 2017-09-25 20:38:57Z thoar@ucar.edu $
+
+! BEGIN DART PREPROCESS KIND LIST
+! AIRNOW_CO,                       KIND_CO,                     COMMON_CODE
+! AIRNOW_SO2,                      KIND_SO2,                    COMMON_CODE
+! AIRNOW_NO2,                      KIND_NO2,                    COMMON_CODE
+! AIRNOW_NOy,                      KIND_NOy,                    COMMON_CODE
+! AIRNOW_O3,                       KIND_O3,                     COMMON_CODE
+! AIRNOW_PM10,                     KIND_PM10,                   COMMON_CODE
+! AIRNOW_PM25,                     KIND_PM25,                   COMMON_CODE
+! AIRNOW_PB,                       KIND_PB,                     COMMON_CODE
+! AIRNOW_NMOC,                     KIND_NMOC,                   COMMON_CODE
+! AIRNOW_PRESSURE,                 KIND_PRESSURE,               COMMON_CODE
+! AIRNOW_TEMPERATURE,              KIND_TEMPERATURE,            COMMON_CODE
+! AIRNOW_RELATIVE_HUMIDITY,        KIND_RELATIVE_HUMIDITY,      COMMON_CODE
+! AIRNOW_U_WIND_COMPONENT,         KIND_U_WIND_COMPONENT,       COMMON_CODE
+! AIRNOW_V_WIND_COMPONENT,         KIND_V_WIND_COMPONENT,       COMMON_CODE
+! END DART PREPROCESS KIND LIST
+
+! <next few lines under version control, do not edit>
+! $URL: https://svn-dares-dart.cgd.ucar.edu/DART/tags/wrf-chem.r13172/obs_def/obs_def_AIRNOW_OBS_mod.f90 $
+! $Id: obs_def_AIRNOW_OBS_mod.f90 11954 2017-09-25 20:38:57Z thoar@ucar.edu $
+! $Revision: 11954 $
+! $Date: 2017-09-25 14:38:57 -0600 (Mon, 25 Sep 2017) $
