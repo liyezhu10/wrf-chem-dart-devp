e951584e5d9c79cc5607c290534586d27f08b0c5
Kate Fossell <fossell@ucar.edu>
2019-10-18 14:48:13 -0600

    Add more chem forward operator routines

---
+++ observations/forward_operators/obs_def_PANDA_OBS_mod.f90
@@ -0,0 +1,28 @@
+! DART software - Copyright 2004 - 2013 UCAR. This open source software is
+! provided by UCAR, "as is", without charge, subject to all terms of use at
+! http://www.image.ucar.edu/DAReS/DART/DART_download
+!
+! DART $Id: obs_def_PANDA_OBS_mod.f90 11954 2017-09-25 20:38:57Z thoar@ucar.edu $
+
+! BEGIN DART PREPROCESS KIND LIST
+! PANDA_CO,                       QTY_CO,                     COMMON_CODE
+! PANDA_SO2,                      QTY_SO2,                    COMMON_CODE
+! PANDA_NO2,                      QTY_NO2,                    COMMON_CODE
+! PANDA_NOy,                      QTY_NOy,                    COMMON_CODE
+! PANDA_O3,                       QTY_O3,                     COMMON_CODE
+! PANDA_PM10,                     QTY_PM10,                   COMMON_CODE
+! PANDA_PM25,                     QTY_PM25,                   COMMON_CODE
+! PANDA_PB,                       QTY_PB,                     COMMON_CODE
+! PANDA_NMOC,                     QTY_NMOC,                   COMMON_CODE
+! PANDA_PRESSURE,                 QTY_PRESSURE,               COMMON_CODE
+! PANDA_TEMPERATURE,              QTY_TEMPERATURE,            COMMON_CODE
+! PANDA_RELATIVE_HUMIDITY,        QTY_RELATIVE_HUMIDITY,      COMMON_CODE
+! PANDA_U_WIND_COMPONENT,         QTY_U_WIND_COMPONENT,       COMMON_CODE
+! PANDA_V_WIND_COMPONENT,         QTY_V_WIND_COMPONENT,       COMMON_CODE
+! END DART PREPROCESS KIND LIST
+
+! <next few lines under version control, do not edit>
+! $URL: https://svn-dares-dart.cgd.ucar.edu/DART/tags/wrf-chem.r13172/obs_def/obs_def_PANDA_OBS_mod.f90 $
+! $Id: obs_def_PANDA_OBS_mod.f90 11954 2017-09-25 20:38:57Z thoar@ucar.edu $
+! $Revision: 11954 $
+! $Date: 2017-09-25 14:38:57 -0600 (Mon, 25 Sep 2017) $
